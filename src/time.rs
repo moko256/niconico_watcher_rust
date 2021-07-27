ada63295b21ac064ec99f0a2f7a0be02d2c51ef0
moko256 <koutaro.mo@gmail.com>
2021-07-27 20:28:29 +0900

    chore: use duration directly

--- src/time.rs
+++ src/time.rs
@@ -1,20 +1,15 @@
 use log::error;
-use std::convert::TryFrom;
 
 use chrono::DateTime;
 use chrono::Utc;
 
 use tokio::time::sleep;
-use tokio::time::Duration;
 
 pub async fn wait_until(when: DateTime<Utc>) {
-    let until = when - Utc::now();
-    let wait_s = until.num_seconds();
-    let wait_s = TryFrom::try_from(wait_s);
-    match wait_s {
-        Ok(wait_s) => {
-            let wait_s = Duration::from_secs(wait_s);
-            sleep(wait_s).await;
+    let wait_until = when - Utc::now();
+    match wait_until.to_std() {
+        Ok(wait_until) => {
+            sleep(wait_until).await;
         }
         Err(_err) => {
             error!(target: "nicow", "time: Schedule was gone. Skipped.");
