f9d921833c32db6bfa717bf0572b9b489f458564
moko256 <koutaro.mo@gmail.com>
2022-03-23 15:10:57 +0900

    chore: add test

--- src/model.rs
+++ src/model.rs
@@ -21,6 +21,11 @@ impl State {
         });
     }
 
+    fn movie_postable(target: &NicoVideo, previous: &Vec<NicoVideo>) -> bool {
+        return State::movie_not_contains_in_prev(target, previous)
+            && State::movie_newer_than_oldest_prev(target, previous);
+    }
+
     // Note: this algorithm does not consider when getVideos().len() > api query limit
     pub async fn next_state(&mut self, repo: &mut impl Repo) {
         let data = repo.get_videos().await;
@@ -30,9 +35,7 @@ impl State {
                     // Exclude when
                     // - The movie is already posted.
                     // - The movie have forgotten whether posted, but older than the oldest in remembered.
-                    if State::movie_not_contains_in_prev(n, &previous)
-                        && State::movie_newer_than_oldest_prev(n, &previous)
-                    {
+                    if State::movie_postable(n, previous) {
                         repo.post_message(n).await;
                     }
                 }
@@ -55,27 +58,31 @@ mod tests {
 
     use super::*;
 
-    fn test_data() -> (NicoVideo, NicoVideo, NicoVideo) {
-        let mut test_data = Vec::new();
+    fn test_datum() -> (NicoVideo, NicoVideo, NicoVideo) {
+        let mut test_datum = Vec::new();
         for i in 0..3 {
-            test_data.push(NicoVideo {
-                title: String::new(),
-                content_id: format!("sm{}", i),
-                start_time: DateTime::parse_from_rfc3339(&format!("2022-02-1{}T00:00:00Z", i + 3))
-                    .unwrap()
-                    .with_timezone(&Utc),
-            });
+            test_datum.push(test_data(i));
         }
-        let newer = test_data.pop().unwrap();
-        let n = test_data.pop().unwrap();
-        let older = test_data.pop().unwrap();
+        let newer = test_datum.pop().unwrap();
+        let n = test_datum.pop().unwrap();
+        let older = test_datum.pop().unwrap();
         return (older, n, newer);
+    }
+
+    fn test_data(i: i32) -> NicoVideo {
+        return NicoVideo {
+            title: String::new(),
+            content_id: format!("sm{}", i),
+            start_time: DateTime::parse_from_rfc3339(&format!("2022-02-1{}T00:00:00Z", i + 3))
+                .unwrap()
+                .with_timezone(&Utc),
+        };
     }
 
     #[test]
     fn test_movie_not_contains_in_prev() {
-        let (older, nb, newer) = test_data();
-        let (_, n, _) = test_data();
+        let (older, nb, newer) = test_datum();
+        let (_, n, _) = test_datum();
         let contains = false;
         let not_contains = true;
         assert_eq!(State::movie_not_contains_in_prev(&n, &vec![]), not_contains);
@@ -92,13 +99,45 @@ mod tests {
 
     #[test]
     fn test_movie_newer_than_oldest_prev() {
-        let (older, nb, newer) = test_data();
-        let (_, n, _) = test_data();
+        let (older, nb, newer) = test_datum();
+        let (_, n, _) = test_datum();
         let old = false;
         let new = true;
         assert_eq!(State::movie_newer_than_oldest_prev(&n, &vec![]), new);
         assert_eq!(State::movie_newer_than_oldest_prev(&n, &vec![older]), new);
         assert_eq!(State::movie_newer_than_oldest_prev(&n, &vec![nb]), old);
         assert_eq!(State::movie_newer_than_oldest_prev(&n, &vec![newer]), old);
+    }
+
+    #[test]
+    fn test_movie_postable() {
+        let assert = |prev: Vec<NicoVideo>, next: Vec<NicoVideo>, expected: Vec<NicoVideo>| {
+            let mut r = Vec::new();
+            for n in next {
+                if State::movie_postable(&n, &prev) {
+                    r.push(n);
+                }
+            }
+            for (a, e) in r.iter().zip(expected) {
+                assert_eq!(e.content_id, a.content_id);
+            }
+        };
+
+        assert(
+            vec![test_data(1)],
+            vec![test_data(1), test_data(2)],
+            vec![test_data(2)],
+        );
+        assert(
+            vec![test_data(1), test_data(3)],
+            vec![test_data(1), test_data(2), test_data(3)],
+            vec![test_data(2)],
+        );
+        assert(vec![test_data(1), test_data(2)], vec![test_data(1)], vec![]);
+        assert(
+            vec![test_data(1), test_data(2)],
+            vec![test_data(3)],
+            vec![test_data(3)],
+        );
     }
 }
